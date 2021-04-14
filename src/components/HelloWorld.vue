c77e28b2e7ff36f3d958945d00aff8e4d24f2ffb
Joao Gabriel <jgdomingues@protonmail.com>
2021-04-14 20:55:42 -0300

    Make our new VueJS app look a little nice
    Changed the initial files to try to match the old style

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,30 +1,26 @@
 <template>
-  <h1>{{ msg }}</h1>
+	<header>
+		<h1>{{ msg }}</h1>
+		<p>Hi, my name is John Gabriel.</p>
+	</header>
 
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Documentation</a> |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
+	<main>
+		<p>Main content.</p>
+    </main>
 
-  <button @click="state.count++">count is: {{ state.count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+	<footer>
+		<p>2021</p>
+	</footer>
 </template>
 
 <script setup>
-import { defineProps, reactive } from 'vue'
+import { defineProps } from 'vue'
 
 defineProps({
-  msg: String
+	msg: String
 })
 
-const state = reactive({ count: 0 })
 </script>
 
 <style scoped>
-a {
-  color: #42b983;
-}
 </style>
